9b68a6bcd4ff29e0645a195d9c63ccc656739d15
sergey <sergey.fomin.90@inbox.ru>
2022-09-28 22:58:04 +0400

    changes

--- Sem1/Home5.py
+++ Sem1/Home5.py
@@ -8,5 +8,4 @@ xA = int(input("Введите координату x, точки A:  "))
 yA = int(input("Введите координату y, точки A:  "))
 xB = int(input("Введите координату x, точки B:  "))
 yB = int(input("Введите координату x, точки B:  "))
-distanceAB = ((xB - xA)**2 + (yB - yA)**2)**0.5
-print (f"{(distanceAB):.3f}")
+print (f"{((xB - xA)**2 + (yB - yA)**2)**0.5:0.4}")
