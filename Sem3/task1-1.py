4dbe34c1beb6782308524a1d003da8f777387356
sergey <sergey.fomin.90@inbox.ru>
2022-10-01 14:36:16 +0400

    add_3task

---
+++ Sem3/task1-1.py
@@ -0,0 +1,13 @@
+# 1. Задайте список, состоящий из произвольных чисел, количество
+# задаёт пользователь. Напишите программу, которая определит,
+# присутствует ли в заданном списке число, полученное от пользователя.
+from random import sample
+
+def find_num(num, q):
+    q = q if q > 0 else -q
+    arr = sample(range(1, q*2), q)
+    print (arr)
+    if num in arr:
+        return True
+    return False
+print (find_num(10,-10))
