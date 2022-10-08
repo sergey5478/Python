1561049ffdd367c6f69239b6da720bf8d2b5b93d
sergey <sergey.fomin.90@inbox.ru>
2022-10-08 22:27:27 +0400

    add_home

--- Sem5/Home1.py
+++ Sem5/Home1.py
@@ -3,3 +3,20 @@
 # in Number of words: 10
 # out авб абв бав абв вба бав вба абв абв абв
 # авб бав вба бав вба
+import random
+
+def Get_word(number):
+    word = 'абв'
+    result = []
+    for i in range(number):
+        temp = random.sample(word, k = 3)
+        result.append(''.join(temp))     
+    return ' '.join(result)
+
+def Sorting(tabl):
+    tabl = list(filter(lambda x: 'абв' not in x, tabl.split()))
+    return " ".join(tabl)
+
+my_list = Get_word(int(input('in Number of words ')))
+print(my_list)
+print(Sorting(my_list))
--- Sem5/Home2.py
+++ Sem5/Home2.py
@@ -1,5 +1,5 @@
-# 2. Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных.
-# Входные и выходные данные хранятся в отдельных текстовых файлах.
+# 2. Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления 
+# данных. Входные и выходные данные хранятся в отдельных текстовых файлах.
 # Алгоритм RLE
 # in 
 # Enter the name of the file with the text:
@@ -11,3 +11,57 @@
 # out
 # aaaaavvvvvvvvvvvvvvvvvvvvvvvvvvvvvssssDDDdddFFggggOOiiiaa
 # vvvvvvvvvvvbbwwPPuuuTTYyWWQQ
+start = 'aaaaavvvvvvvvvvvvvvvvvvvvvvvvvvvvvssssDDDdddFFggggOOiiiaa\
+vvvvvvvvvvvbbwwPPuuuTTYyWWQQ'
+with open('text_words.txt', 'w', encoding='utf-8') as data1:
+    data1.writelines(start)
+
+with open('text_words.txt', 'r') as data2:
+    data = ''
+    for line in data2:
+        data += line
+    print(data)
+
+def Encode(data): 
+    result = '' 
+    prev_char = '' 
+    a = 1 
+    if not data: return '' 
+    for i in data: 
+        if i != prev_char:                 
+                if prev_char: 
+                    result += str(a) + prev_char 
+                a = 1 
+                prev_char = i 
+        else: 
+            a += 1 
+    else: 
+        result += str(a) + prev_char 
+        return result
+encoded_val = Encode(data) 
+print(encoded_val)
+
+with open('text_code_words.txt', 'w', encoding='utf-8') as data3:
+    data3.writelines(encoded_val)
+
+with open('text_code_words.txt', 'r') as data4:
+    data5 = ''
+    for line in data4:
+        data5 += line
+    print(data5)
+
+def Decode(data):
+    result = ''
+    a = ''
+    for i in data:        
+        if i.isdigit():            
+           a += i 
+        else:            
+            result += i * int(a)
+            a = ''
+    return result
+decoded_val = Decode(data5)
+print(decoded_val)
+
+with open('text_decode_words.txt', 'w', encoding='utf-8') as data33:
+    data33.writelines(decoded_val)
